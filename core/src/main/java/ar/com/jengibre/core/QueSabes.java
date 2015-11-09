3845168b2960fef87bdcbdb97b717f02127a799f
AndresQ <tulsidas@gmail.com>
2015-11-09 20:56:59 -0300

    evdev-java integración con los sensores multitouch (wip)

--- core/src/main/java/ar/com/jengibre/core/QueSabes.java
+++ core/src/main/java/ar/com/jengibre/core/QueSabes.java
@@ -6,8 +6,10 @@ import static playn.core.PlayN.json;
 import static playn.core.PlayN.keyboard;
 import static playn.core.PlayN.pointer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import playn.core.Game;
 import playn.core.GroupLayer;
@@ -21,11 +23,19 @@ import playn.core.Pointer;
 import playn.core.util.Clock;
 import pythagoras.f.Point;
 
-public class QueSabes extends Game.Default {
+import com.dgis.input.evdev.EventDevice;
+import com.dgis.input.evdev.InputEvent;
+import com.dgis.input.evdev.InputListener;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+public class QueSabes extends Game.Default implements InputListener {
 
    private static final int UPDATE_RATE = 40;
 
    private Clock.Source clock = new Clock.Source(UPDATE_RATE);
+
+   private EventDevice mt;
 
    // private Sector norte, sur, este, oeste;
 
@@ -41,6 +51,15 @@ public class QueSabes extends Game.Default {
 
    public QueSabes() {
       super(UPDATE_RATE); // 24 FPS
+
+      try {
+         mt = new EventDevice("/dev/input/event18");
+         mt.addListener(this);
+      }
+      catch (IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
    }
 
    @Override
@@ -196,5 +215,89 @@ public class QueSabes extends Game.Default {
       return sur;
       // }
       // }
+   }
+
+   // ////////////////////////////////////////////////////////////////
+   // evdev InputListener
+   // ////////////////////////////////////////////////////////////////
+   private static final short SYN_REPORT = 0;
+
+   private static final short SYN_CONFIG = 1;
+
+   private static final short BTN_TOUCH = 0x14a;
+
+   private static final short ABS_MT_SLOT = 0x2f;
+
+   private static final short ABS_MT_TOUCH_MAJOR = 0x30;
+
+   private static final short ABS_MT_TOUCH_MINOR = 0x31;
+
+   private static final short ABS_MT_ORIENTATION = 0x34;
+
+   private static final short ABS_MT_POSITION_X = 0x35;
+
+   private static final short ABS_MT_POSITION_Y = 0x36;
+
+   private static final short ABS_MT_TRACKING_ID = 0x39;
+
+   ImmutableMap<Short, String> EVENTS = ImmutableMap
+         .<Short, String> builder()
+         .put(SYN_REPORT, "SYN_REPORT")
+         // .put(SYN_CONFIG, "SYN_CONFIG")
+         // .put(BTN_TOUCH, "BTN_TOUCH")
+         .put(ABS_MT_SLOT, "ABS_MT_SLOT")
+         // .put(ABS_MT_TOUCH_MAJOR, "ABS_MT_TOUCH_MAJOR")
+         // .put(ABS_MT_TOUCH_MINOR, "ABS_MT_TOUCH_MINOR")
+         // .put(ABS_MT_ORIENTATION, "ABS_MT_ORIENTATION")
+         .put(ABS_MT_POSITION_X, "ABS_MT_POSITION_X").put(ABS_MT_POSITION_Y, "ABS_MT_POSITION_Y")
+         .put(ABS_MT_TRACKING_ID, "ABS_MT_TRACKING_ID").build();
+
+   // <slot, tracking id>
+   private Map<Integer, Point> slots = Maps.newHashMap();
+
+   private int currentSlot = 0;
+
+   public void event(InputEvent e) {
+      String code = EVENTS.get(e.code);
+      if (e.type == 3 && code != null) {
+//         System.out.println(e.type + "," + code + "," + e.value);
+
+         if (e.code == ABS_MT_SLOT) {
+            currentSlot = e.value;
+         }
+         else if (e.code == ABS_MT_TRACKING_ID) {
+            if (e.value == -1) {
+               // XXX onTouchEnd
+               System.out.println("onTouchEnd " + currentSlot);
+
+               slots.remove(currentSlot);
+            }
+            else {
+               // XXX onTouchStart
+               System.out.println("onTouchStart " + currentSlot);
+            }
+         }
+         else if (e.code == ABS_MT_POSITION_X) {
+            getCurrentPoint(currentSlot).x = e.value;
+         }
+         else if (e.code == ABS_MT_POSITION_Y) {
+            getCurrentPoint(currentSlot).y = e.value;
+         }
+         else if (e.code == SYN_REPORT) {
+            // aviso que se movio cada uno de los slots
+            // XXX onTouchMove
+            for (Map.Entry<Integer, Point> entry : slots.entrySet()) {
+               Point p = entry.getValue();
+               System.out.println("onTouchMove " + entry.getKey() + "(" + p.x + "," + p.y + ")");
+            }
+         }
+      }
+   }
+
+   private Point getCurrentPoint(int slot) {
+      Point p = slots.getOrDefault(currentSlot, new Point());
+      slots.put(currentSlot, p);
+
+      return p;
    }
 }
